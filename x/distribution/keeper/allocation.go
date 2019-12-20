7da5b8cae00460216bbf0f6c0624f0da213f7fa3
Roman Shtylman <roman@polychain.capital>
2019-12-20 10:06:50 -0800

    add distribution keeper allocation hooks

--- x/distribution/keeper/allocation.go
+++ x/distribution/keeper/allocation.go
@@ -100,7 +100,7 @@ func (k Keeper) AllocateTokens(
 }
 
 // AllocateTokensToValidator allocate tokens to a particular validator, splitting according to commission
-func (k Keeper) AllocateTokensToValidator(ctx sdk.Context, val exported.ValidatorI, tokens sdk.DecCoins) {
+func (k *Keeper) AllocateTokensToValidator(ctx sdk.Context, val exported.ValidatorI, tokens sdk.DecCoins) {
 	// split tokens between validator and delegators according to commission
 	commission := tokens.MulDec(val.GetCommission())
 	shared := tokens.Sub(commission)
@@ -133,4 +133,6 @@ func (k Keeper) AllocateTokensToValidator(ctx sdk.Context, val exported.Validato
 	outstanding := k.GetValidatorOutstandingRewards(ctx, val.GetOperator())
 	outstanding = outstanding.Add(tokens)
 	k.SetValidatorOutstandingRewards(ctx, val.GetOperator(), outstanding)
+
+	k.hooks.AfterAllocateTokensToValidator(ctx, val, commission, shared)
 }

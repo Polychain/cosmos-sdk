867e34c4faf5652e07aa8cb3475cf8e8678ce668
Roman Shtylman <roman@polychain.capital>
2020-01-04 10:03:44 -0800

    change AllocateTokensToValidator to use regular receiver rather than pointer

--- x/distribution/keeper/allocation.go
+++ x/distribution/keeper/allocation.go
@@ -100,7 +100,7 @@ func (k Keeper) AllocateTokens(
 }
 
 // AllocateTokensToValidator allocate tokens to a particular validator, splitting according to commission
-func (k *Keeper) AllocateTokensToValidator(ctx sdk.Context, val exported.ValidatorI, tokens sdk.DecCoins) {
+func (k Keeper) AllocateTokensToValidator(ctx sdk.Context, val exported.ValidatorI, tokens sdk.DecCoins) {
 	// split tokens between validator and delegators according to commission
 	commission := tokens.MulDec(val.GetCommission())
 	shared := tokens.Sub(commission)
